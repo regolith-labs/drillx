ac5dd5da9f081d6256d64bd7fd3d15a3dcdab033
Hardhat Chad <hardhatchad@gmail.com>
2024-04-25 22:51:32 +0000

    op2

--- drillx/src/operator2.rs
+++ drillx/src/operator2.rs
@@ -1,13 +1,17 @@
 use std::{mem, time::Instant};
 
+use num_enum::{IntoPrimitive, TryFromPrimitive};
 #[cfg(not(feature = "solana"))]
 use sha3::{Digest, Keccak256};
 
 /// Digest size
-const D: usize = 16;
+// const D: usize = 16;
 
 /// Number of recursive reads per loop
-const R: usize = 2048;
+const READS: usize = 1024;
+
+/// Number of recursive ops per loop
+const OPS: usize = 512;
 
 /// Global state for drilling algorithm
 pub struct Operator2<'a> {
@@ -17,82 +21,69 @@ pub struct Operator2<'a> {
 
 impl<'a> Operator2<'a> {
     pub fn new(challenge: &'a [u8; 32], nonce: &'a [u8; 8], noise: &'a [u8]) -> Operator2<'a> {
-        let state;
-        #[cfg(not(feature = "solana"))]
-        {
-            Keccak256::new()
-                .chain_update(&challenge.as_slice())
-                .chain_update(&nonce.as_slice())
-                .finalize_into((&mut state).into());
-        }
-
-        #[cfg(feature = "solana")]
-        {
-            state = solana_program::keccak::hashv(&[&challenge.as_slice(), &nonce.as_slice()]).0;
-        }
-
         Operator2 {
             noise: as_usize_slice(noise).expect("Failed to read noise as &[usize]"),
-            state,
+            state: solana_program::keccak::hashv(&[&challenge.as_slice(), &nonce.as_slice()]).0,
         }
     }
 
     /// Build digest using unpredictable and non-parallelizable operations
-    pub fn drill(&mut self) -> [u8; D] {
-        let mut digest = [0; D];
+    pub fn drill(&mut self) -> [u8; 32] {
         let mut hash_time = 0;
-        let mut index_time = 0;
-        for i in 0..D / 8 {
+        let mut read_time = 0;
+        let mut op_time = 0;
+        let mut r = usize::from_le_bytes([
+            self.state(0),
+            self.state(4),
+            self.state(8),
+            self.state(12),
+            self.state(16),
+            self.state(20),
+            self.state(24),
+            self.state(28),
+        ]);
+        for i in 0..4 {
+            // Fetch noise
+            let read_timer = Instant::now();
+            let idx = self.indices();
+            for j in 0..8 {
+                self.state[8 * i + j] ^= self.do_reads::<READS>(idx[j], r);
+            }
+            read_time += read_timer.elapsed().as_nanos();
+
+            // Do ops
+            let op_timer = Instant::now();
+            for j in 0..OPS {
+                r ^= self.op(idx[j % 8], r, j);
+            }
+            op_time += op_timer.elapsed().as_nanos();
+
             // Hash state
             let hash_timer = Instant::now();
-            self.hash();
+            self.hash(r);
             hash_time += hash_timer.elapsed().as_nanos();
-
-            // Fetch noise
-            let idx = self.indices();
-            let index_timer = Instant::now();
-            digest[8 * i + 0] = self.recurse::<R>(idx[0]);
-            digest[8 * i + 1] = self.recurse::<R>(idx[1]);
-            digest[8 * i + 2] = self.recurse::<R>(idx[2]);
-            digest[8 * i + 3] = self.recurse::<R>(idx[3]);
-            digest[8 * i + 4] = self.recurse::<R>(idx[4]);
-            digest[8 * i + 5] = self.recurse::<R>(idx[5]);
-            digest[8 * i + 6] = self.recurse::<R>(idx[6]);
-            digest[8 * i + 7] = self.recurse::<R>(idx[7]);
-            index_time += index_timer.elapsed().as_nanos();
         }
 
         println!(
-            "eight recurse: hash {hash_time} vs index {index_time}: {}x",
-            index_time as f64 / hash_time as f64
+            "op {op_time} hash {hash_time} vs read {read_time}: {}x {}x",
+            read_time as f64 / hash_time as f64,
+            read_time as f64 / op_time as f64
         );
 
-        digest
+        self.state
     }
 
     #[inline(always)]
-    fn hash(&mut self) {
-        #[cfg(not(feature = "solana"))]
-        {
-            Keccak256::new()
-                .chain_update(&self.state)
-                .finalize_into((&mut self.state).into());
-        }
-
-        #[cfg(feature = "solana")]
-        {
-            self.state = solana_program::keccak::hashv(&[&self.state]).0;
-        }
+    fn hash(&mut self, r: usize) {
+        self.state = solana_program::keccak::hashv(&[&self.state, r.to_le_bytes().as_slice()]).0;
     }
 
     #[inline(always)]
-    fn recurse<const R: usize>(&self, mut index: usize) -> u8 {
-        let oi = index;
+    fn do_reads<const R: usize>(&mut self, mut index: usize, r: usize) -> u8 {
         for _ in 0..R {
             index ^= self.noise(index);
-            // TODO stateful op here
         }
-        (self.noise(index) >> (oi % 8)) as u8
+        (self.noise(index) >> (r % 8)) as u8
     }
 
     #[inline(always)]
@@ -101,13 +92,18 @@ impl<'a> Operator2<'a> {
     }
 
     #[inline(always)]
-    pub fn indices(&self) -> [usize; 8] {
+    fn state(&self, i: usize) -> u8 {
+        unsafe { *self.state.get_unchecked(i % 32) }
+    }
+
+    #[inline(always)]
+    fn indices(&self) -> [usize; 8] {
         core::array::from_fn(|i| {
             usize::from_le_bytes([
-                self.state[4 * i],
-                self.state[4 * i + 1],
-                self.state[4 * i + 2],
-                self.state[4 * i + 3],
+                self.state(4 * i),
+                self.state(4 * i + 1),
+                self.state(4 * i + 2),
+                self.state(4 * i + 3),
                 0,
                 0,
                 0,
@@ -115,10 +111,62 @@ impl<'a> Operator2<'a> {
             ])
         })
     }
+
+    /// Select an opcode from the current state
+    #[inline]
+    fn opcode(&self, opcount: usize, b: usize) -> Opcode {
+        Opcode::try_from(((opcount ^ b) % Opcode::cardinality()) as u8).expect("Unknown opcode")
+    }
+
+    /// Execute a random operation the given operands
+    #[inline]
+    fn op(&self, a: usize, b: usize, opcount: usize) -> usize {
+        match self.opcode(opcount, b) {
+            Opcode::Add => a.wrapping_add(b),
+            Opcode::Sub => a.wrapping_sub(b),
+            Opcode::Mul => a.wrapping_mul(b),
+            Opcode::Div => {
+                if a.gt(&b) {
+                    a.wrapping_div(b.saturating_add(2))
+                } else {
+                    b.wrapping_div(a.saturating_add(2))
+                }
+            }
+            Opcode::Xor => a ^ b,
+            Opcode::Right => a >> (b % 64),
+            Opcode::Left => a << (b % 64),
+        }
+    }
 }
 
+/// Set of arbitrary compute operations to chose from
+// TODO Add more ops
+#[derive(Debug, IntoPrimitive, TryFromPrimitive)]
+#[repr(u8)]
+enum Opcode {
+    Add = 0,
+    Sub,
+    Mul,
+    Div,
+    Xor,
+    Right,
+    Left,
+    // Nop,
+    // Swap,
+    // FAdd,
+    // FSub,
+    // FMul,
+    // FSqrt,
+}
+
+impl Opcode {
+    pub fn cardinality() -> usize {
+        7
+    }
+}
+
+// Check if the slice is properly aligned and sized
 fn as_usize_slice(bytes: &[u8]) -> Option<&[usize]> {
-    // Check if the slice is properly aligned and sized
     let align = mem::align_of::<usize>();
     let size = mem::size_of::<usize>();
     if bytes.as_ptr() as usize % align == 0 && bytes.len() % size == 0 {
