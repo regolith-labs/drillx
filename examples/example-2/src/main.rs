9b32322f9b9298dc1067b3ce9397ca5691c239b0
Hardhat Chad <hardhatchad@gmail.com>
2024-04-26 17:21:19 +0000

    feature flags

--- examples/example-2/src/main.rs
+++ examples/example-2/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let challenge = [255; 32];
 
     // Read noise file.
-    let mut file = File::open("../../noise.txt").unwrap();
+    let mut file = File::open("noise.txt").unwrap();
     let mut buffer = Vec::new();
     file.read_to_end(&mut buffer).unwrap();
     let noise = buffer.as_slice();
