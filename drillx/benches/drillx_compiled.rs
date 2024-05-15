fed78377a9f44d0f702cebb4e4edc0d3c3c1bdad
Shady Khalifa <dev+github@shadykhalifa.me>
2024-05-15 22:44:32 +0300

    add benches to drillx with small improvements
    Also added Nix and flakes for local develop.

---
+++ drillx/benches/drillx_compiled.rs
@@ -0,0 +1,28 @@
+use criterion::BenchmarkId;
+use criterion::Criterion;
+use criterion::Throughput;
+use criterion::{criterion_group, criterion_main};
+
+fn drillx_compiled(nonces: u64) {
+    let mut memory = drillx::equix::SolverMemory::new();
+    let challenge = [255; 32];
+    for nonce in 0..nonces {
+        drillx::hash_with_shared_memory(&mut memory, &challenge, &nonce.to_le_bytes()).ok();
+    }
+}
+
+fn different_sizes(c: &mut Criterion) {
+    static KH: usize = 1000;
+
+    let mut group = c.benchmark_group("from_elem");
+    for size in [KH, 2 * KH, 4 * KH].iter() {
+        group.throughput(Throughput::Elements(*size as u64));
+        group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
+            b.iter(|| drillx_compiled(size as u64))
+        });
+    }
+    group.finish();
+}
+
+criterion_group!(benches, different_sizes);
+criterion_main!(benches);
--- drillx/src/lib.rs
+++ drillx/src/lib.rs
@@ -1,6 +1,25 @@
+/// Re-export the equix crate
+pub use equix;
+
 /// Generates a new drillx hash from a challenge and nonce
 pub fn hash(challenge: &[u8; 32], nonce: &[u8; 8]) -> Result<Hash, DrillxError> {
     let digest = build_digest(challenge, nonce)?;
+    Ok(Hash {
+        d: digest,
+        h: hashv(&digest, nonce),
+    })
+}
+
+/// Generates a new drillx hash from a challenge and nonce
+#[inline(always)]
+pub fn hash_with_shared_memory(
+    memory: &mut equix::SolverMemory,
+    challenge: &[u8; 32],
+    nonce: &[u8; 8],
+) -> Result<Hash, DrillxError> {
+    // Seed
+    let seed = construct_seed(challenge, nonce);
+    let digest = build_digest_with_shared_memory(memory, &seed)?;
     Ok(Hash {
         d: digest,
         h: hashv(&digest, nonce),
@@ -15,7 +34,22 @@ fn seed(challenge: &[u8; 32], nonce: &[u8; 8]) -> [u8; 40] {
     buf
 }
 
-/// Constructs a keccak digest from a challenge and nonce using equix hashes
+/// Concatenates two arrays into a single array
+#[inline(always)]
+fn construct_seed(a: &[u8; 32], b: &[u8; 8]) -> [u8; 40] {
+    let mut result = std::mem::MaybeUninit::uninit();
+    let dest = result.as_mut_ptr() as *mut u8;
+    // SAFETY: `dest` is valid for `40` elements.
+    // SAFETY: `a` and `b` are valid for `32` and `8` elements respectively.
+    // SAFETY: `a` and `b` are non-overlapping with `dest`.
+    unsafe {
+        core::ptr::copy_nonoverlapping(a.as_ptr(), dest, 32);
+        core::ptr::copy_nonoverlapping(b.as_ptr(), dest.add(32), 8);
+        result.assume_init()
+    }
+}
+
+/// Constructs a blake3 digest from a challenge and nonce using equix hashes
 fn build_digest(challenge: &[u8; 32], nonce: &[u8; 8]) -> Result<[u8; 16], DrillxError> {
     // Seed
     let seed = seed(challenge, nonce);
@@ -29,9 +63,27 @@ fn build_digest(challenge: &[u8; 32], nonce: &[u8; 8]) -> Result<[u8; 16], Drill
     };
 
     // Digest
-    let mut digest = [0u8; 16];
-    digest.copy_from_slice(solution.to_bytes().as_slice());
-    Ok(digest)
+    Ok(solution.to_bytes())
+}
+
+/// Constructs a blake3 digest from a challenge and nonce using equix hashes
+#[inline(always)]
+fn build_digest_with_shared_memory(
+    memory: &mut equix::SolverMemory,
+    seed: &[u8],
+) -> Result<[u8; 16], DrillxError> {
+    let equix = equix::EquiXBuilder::new()
+        .runtime(equix::RuntimeOption::CompileOnly)
+        .build(&seed)
+        .map_err(|_| DrillxError::BadEquix)?;
+
+    // Equix
+    let solutions = equix.solve_with_memory(memory);
+    // SAFETY: The equix solver guarantees that the first solution is always valid
+    let solution = unsafe { solutions.get_unchecked(0) };
+
+    // Digest
+    Ok(solution.to_bytes())
 }
 
 /// Returns true if the digest if valid equihash construction from the challenge and nonce
@@ -41,8 +93,19 @@ pub fn is_valid_digest(challenge: &[u8; 32], nonce: &[u8; 8], digest: &[u8; 16])
 }
 
 /// Calculates a hash from the provided digest and nonce
+#[cfg(all(feature = "program", not(feature = "native")))]
 fn hashv(digest: &[u8; 16], nonce: &[u8; 8]) -> [u8; 32] {
     solana_program::blake3::hashv(&[&digest.as_slice(), &nonce.as_slice()]).to_bytes()
+}
+
+/// Calculates a hash from the provided digest and nonce
+#[cfg(all(feature = "native", not(feature = "program")))]
+fn hashv(digest: &[u8; 16], nonce: &[u8; 8]) -> [u8; 32] {
+    // Hash an input incrementally.
+    let mut hasher = blake3::Hasher::new();
+    hasher.update(digest);
+    hasher.update(nonce);
+    hasher.finalize().into()
 }
 
 /// Returns the number of leading zeros on a 32 byte buffer
