e78dfd3bd8850fb3d28a187586ed33d066c34852
Hardhat Chad <hardhatchad@gmail.com>
2024-05-16 21:12:22 +0000

    test

--- drillx/src/cuda.rs
+++ drillx/src/cuda.rs
@@ -2,13 +2,18 @@ extern "C" {
     pub fn hash(challenge: *const u8, nonce: *const u8, out: *mut u8);
 }
 
-#[test]
-fn test_gpu() {
-    let challenge = [1; 32];
-    let nonce = [2; 8];
-    let mut out = [0; 16];
-    unsafe {
-        super::cuda::hash(challenge.as_ptr(), nonce.as_ptr(), out.as_mut_ptr());
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_gpu() {
+        let challenge = [1; 32];
+        let nonce = [2; 8];
+        let mut out = [0; 16];
+        unsafe {
+            hash(challenge.as_ptr(), nonce.as_ptr(), out.as_mut_ptr());
+        }
+        assert_eq!(42, u128::from_le_bytes(out));
     }
-    assert_eq!(42, u128::from_le_bytes(out));
 }
