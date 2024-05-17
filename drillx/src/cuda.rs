a47ebc6d3ae0a7285130fe911edc8bf5e0dbad2f
Hardhat Chad <hardhatchad@gmail.com>
2024-05-17 22:39:36 +0000

    cuda

--- drillx/src/cuda.rs
+++ drillx/src/cuda.rs
@@ -5,19 +5,16 @@ extern "C" {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::time::Instant;
 
     #[test]
     fn test_gpu() {
         let challenge = [255; 32];
         let nonce = [2; 8];
         let mut digest = [0; 16];
-        // let timer = Instant::now();
         unsafe {
             hash(challenge.as_ptr(), nonce.as_ptr(), digest.as_mut_ptr());
         }
         let solution = crate::Solution::new(digest, nonce);
-        // println!("Did 1 hash in {} ns", timer.elapsed().as_nanos());
-        assert!(!solution.is_valid(&challenge));
+        assert!(solution.is_valid(&challenge));
     }
 }
