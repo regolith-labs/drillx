88201c6490a6665b914c48e003dea54bcf7e8072
Hardhat Chad <hardhatchad@gmail.com>
2024-05-17 02:46:29 +0000

    copy equix hashx files

--- drillx/build.rs
+++ drillx/build.rs
@@ -4,44 +4,46 @@ fn main() {}
 #[cfg(feature = "cuda")]
 fn main() {
     println!("cargo:rerun-if-changed=cuda/");
-    println!("cargo:rerun-if-changed=equix/");
-    println!("cargo:rerun-if-changed=equix/hashx/");
     println!("cargo:rerun-if-changed=src/");
 
     // Compile hashx C code
     cc::Build::new()
-        .include("equix/hashx/include")
-        .include("equix/hashx/src")
-        .file("equix/hashx/src/blake2.c")
-        .file("equix/hashx/src/compiler.c")
-        .file("equix/hashx/src/compiler_a64.c")
-        .file("equix/hashx/src/compiler_x86.c")
-        .file("equix/hashx/src/context.c")
-        .file("equix/hashx/src/hashx.c")
-        .file("equix/hashx/src/hashx_thread.c")
-        .file("equix/hashx/src/hashx_time.c")
-        .file("equix/hashx/src/program.c")
-        .file("equix/hashx/src/program_exec.c")
-        .file("equix/hashx/src/siphash.c")
-        .file("equix/hashx/src/siphash_rng.c")
-        .file("equix/hashx/src/virtual_memory.c")
+        .cuda(true)
+        .compiler("nvcc")
+        .include("cuda/equix/hashx/include")
+        .include("cuda/equix/hashx/src")
+        .file("cuda/equix/hashx/src/blake2.c")
+        .file("cuda/equix/hashx/src/compiler.c")
+        .file("cuda/equix/hashx/src/compiler_a64.c")
+        .file("cuda/equix/hashx/src/compiler_x86.c")
+        .file("cuda/equix/hashx/src/context.c")
+        .file("cuda/equix/hashx/src/hashx.c")
+        .file("cuda/equix/hashx/src/hashx_thread.c")
+        .file("cuda/equix/hashx/src/program.c")
+        .file("cuda/equix/hashx/src/program_exec.c")
+        .file("cuda/equix/hashx/src/siphash.c")
+        .file("cuda/equix/hashx/src/siphash_rng.c")
+        .file("cuda/equix/hashx/src/virtual_memory.c")
         .compile("hashx.a");
 
     // Compile equix C code
     cc::Build::new()
-        .include("equix/include")
-        .include("equix/src")
-        .include("equix/hashx/include")
-        .include("equix/hashx/src")
-        .file("equix/src/context.c")
-        .file("equix/src/equix.c")
-        .file("equix/src/solver.c")
+        .cuda(true)
+        .compiler("nvcc")
+        .include("cuda/equix/include")
+        .include("cuda/equix/src")
+        .include("cuda/equix/hashx/include")
+        .include("cuda/equix/hashx/src")
+        .file("cuda/equix/src/context.c")
+        .file("cuda/equix/src/equix.c")
+        .file("cuda/equix/src/solver.c")
         .compile("equix.a");
 
+    // Compile drillx
     cc::Build::new()
         .cuda(true)
-        .include("equix/include")
-        .include("equix/hashx/include")
+        .include("cuda/equix/include")
+        .include("cuda/equix/hashx/include")
         .file("cuda/drillx.cu")
         .flag("-cudart=static")
         // .flag("-gencode=arch=compute_89,code=sm_89") // Optimize for RTX 4090
---
+++ drillx/cuda/equix/src/equix.c
@@ -0,0 +1,28 @@
+/* Copyright (c) 2020 tevador <tevador@gmail.com> */
+/* See LICENSE for licensing information */
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include <equix.h>
+#include <hashx.h>
+#include "context.h"
+#include "solver.h"
+#include <hashx_endian.h>
+
+__device__ int equix_solve(
+	equix_ctx* ctx,
+	const void* challenge,
+	size_t challenge_size,
+	equix_solution output[EQUIX_MAX_SOLS])
+{
+	if ((ctx->flags & EQUIX_CTX_SOLVE) == 0) {
+		return 0;
+	}
+
+	if (!hashx_make(ctx->hash_func, challenge, challenge_size)) {
+		return 0;
+	}
+
+	return equix_solver_solve(ctx->hash_func, ctx->heap, output);
+}
