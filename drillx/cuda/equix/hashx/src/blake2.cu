3520f292b06cfbcf9005e7b0e10588009b808d6e
Hardhat Chad <hardhatchad@gmail.com>
2024-05-17 03:38:03 +0000

    device blake2b_IV

--- drillx/cuda/equix/hashx/src/blake2.cu
+++ drillx/cuda/equix/hashx/src/blake2.cu
@@ -15,7 +15,7 @@
 #include "blake2.h"
 #include "hashx_endian.h"
 
-static const uint64_t blake2b_IV[8] = {
+__device__ static const uint64_t blake2b_IV[8] = {
 	UINT64_C(0x6a09e667f3bcc908), UINT64_C(0xbb67ae8584caa73b),
 	UINT64_C(0x3c6ef372fe94f82b), UINT64_C(0xa54ff53a5f1d36f1),
 	UINT64_C(0x510e527fade682d1), UINT64_C(0x9b05688c2b3e6c1f),
