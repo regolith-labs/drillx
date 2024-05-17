83b009e1c202321a3dc60cb9437b3323a8c6a3df
Hardhat Chad <hardhatchad@gmail.com>
2024-05-17 21:01:52 +0000

    cuda

--- drillx/cuda/equix/src/solver.cu
+++ drillx/cuda/equix/src/solver.cu
@@ -54,18 +54,6 @@ __device__ FORCE_INLINE uint64_t hash_value(hashx_ctx* hash_func, equix_idx inde
 	return load64(hash);
 }
 
-__device__ static uint16_t atomicAdd_u16(uint16_t *address, uint16_t val) {
-    unsigned int *base_address = (unsigned int *)((char *)address - ((size_t)address & 2));
-    unsigned int old = atomicAddCustom(base_address, val, ((size_t)address & 2) != 0);
-    return ((old >> (((size_t)address & 2) * 8)) & 0xFFFF);
-}
-
-__device__ static uint16_t atomicSub_u16(uint16_t *address, uint16_t val) {
-	  unsigned int *base_address = (unsigned int *)((char *)address - ((size_t)address & 2));
-    unsigned int old = atomicSubCustom(base_address, (int16_t)val, ((size_t)address & 2) != 0);
-	  return ((old >> (((size_t)address & 2) * 8)) & 0xFFFF);
-}
-
 __device__ static unsigned int atomicAddCustom(unsigned int *address, uint16_t val, bool is_high) {
     unsigned int old, assumed;
     old = *address;
@@ -93,6 +81,19 @@ __device__ static unsigned int atomicSubCustom(unsigned int *address, int16_t va
     } while (assumed != old);
     return old;
 }
+
+__device__ static uint16_t atomicAdd_u16(uint16_t *address, uint16_t val) {
+    unsigned int *base_address = (unsigned int *)((char *)address - ((size_t)address & 2));
+    unsigned int old = atomicAddCustom(base_address, val, ((size_t)address & 2) != 0);
+    return ((old >> (((size_t)address & 2) * 8)) & 0xFFFF);
+}
+
+__device__ static uint16_t atomicSub_u16(uint16_t *address, uint16_t val) {
+	  unsigned int *base_address = (unsigned int *)((char *)address - ((size_t)address & 2));
+    unsigned int old = atomicSubCustom(base_address, (int16_t)val, ((size_t)address & 2) != 0);
+	  return ((old >> (((size_t)address & 2) * 8)) & 0xFFFF);
+}
+
 
 static void build_solution_stage1(equix_idx* output, solver_heap* heap, s2_idx root) {
 	u32 bucket = ITEM_BUCKET(root);
