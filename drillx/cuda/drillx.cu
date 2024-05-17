ff0f3d2ff0f5976ba6bd426cd6d1ca2cb9717c1f
Hardhat Chad <hardhatchad@gmail.com>
2024-05-17 18:09:00 +0000

    cuda

--- drillx/cuda/drillx.cu
+++ drillx/cuda/drillx.cu
@@ -57,7 +57,7 @@ extern "C" void hash(uint8_t *challenge, uint8_t *nonce, uint8_t *out) {
 __global__ void do_solve_stage0(hashx_ctx* hash_func, solver_heap* heap) {
     uint16_t i = blockIdx.x * blockDim.x + threadIdx.x;
     if (i < 65536) {
-        for (j = 0; j < 10; j++) {
+        for (uint16_t j = 0; j < 10; j++) {
             uint64_t value = hash_value(hash_func, i);
         }
         // printf("%d %lld\n", i, value);
