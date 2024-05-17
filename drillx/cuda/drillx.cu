fcccd02fadbbe13fdbcbee1b4036b4282de3d804
Hardhat Chad <hardhatchad@gmail.com>
2024-05-17 19:35:12 +0000

    cuda

--- drillx/cuda/drillx.cu
+++ drillx/cuda/drillx.cu
@@ -45,7 +45,7 @@ extern "C" void hash(uint8_t *challenge, uint8_t *nonce, uint8_t *out) {
     equix_free(ctx);
 
     // Copy results back to host
-    if sols > 0 {
+    if (sols > 0) {
         cudaMemcpy(out, output.idx[0], 16, cudaMemcpyDeviceToHost);        
     }
     // cudaMemcpy(out, d_out, 16, cudaMemcpyDeviceToHost);
