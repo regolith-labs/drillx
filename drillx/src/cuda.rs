5000ada168ed55200998525694c6d39109667eaa
Hardhat Chad <hardhatchad@gmail.com>
2024-05-17 17:57:13 +0000

    cuda

--- drillx/src/cuda.rs
+++ drillx/src/cuda.rs
@@ -5,15 +5,18 @@ extern "C" {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::time::Instant;
 
     #[test]
     fn test_gpu() {
         let challenge = [1; 32];
         let nonce = [2; 8];
         let mut out = [0; 16];
+        let timer = Instant::now();
         unsafe {
             hash(challenge.as_ptr(), nonce.as_ptr(), out.as_mut_ptr());
         }
+        println!("Did 65536 hashx in {} ns", timer.elapsed().as_nanos());
         assert_eq!(42, u128::from_le_bytes(out));
     }
 }
