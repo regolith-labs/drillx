3b8ae79bf2cfaef2407153143976855bc5041535
Hardhat Chad <hardhatchad@gmail.com>
2024-05-18 17:22:31 +0000

    cuda

--- drillx/src/cuda.rs
+++ drillx/src/cuda.rs
@@ -1,7 +1,7 @@
 extern "C" {
     pub static BATCH_SIZE: u32;
     pub fn hash(challenge: *const u8, nonce: *const u8, out: *mut u64);
-    pub fn solve_all_stages(hashes: *u64, out: *mut u8);
+    pub fn solve_all_stages(hashes: *const u64, out: *mut u8);
 }
 
 #[cfg(test)]
