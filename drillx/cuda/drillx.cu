0fc65c955ec44f7b747cb035bf74b5401ba0a573
Hardhat Chad <hardhatchad@gmail.com>
2024-05-18 03:00:19 +0000

    todo

--- drillx/cuda/drillx.cu
+++ drillx/cuda/drillx.cu
@@ -7,12 +7,16 @@
 #include "equix/src/solver.h"
 #include "hashx/src/context.h"
 
+// TODO Add batch size parameter. We will do hashes for (nonce..nonce+batch_size)
 extern "C" void hash(uint8_t *challenge, uint8_t *nonce, uint8_t *out) {
+    // TODO Generate seeds based on batch size
     // Allocate a 40-byte buffer
     uint8_t seed[40];
     memcpy(seed, challenge, 32);
     memcpy(seed + 32, nonce, 8);
 
+    // TODO Allocate array INDEX_SPACE large for hashx values
+    // TODO One array for each hash we are going to offload to gpu
     // Create an equix context
     equix_ctx* ctx = equix_alloc(EQUIX_CTX_SOLVE);
     if (ctx == nullptr) {
@@ -20,17 +24,21 @@ extern "C" void hash(uint8_t *challenge, uint8_t *nonce, uint8_t *out) {
         return;
     }
 
+    // TODO Generate hash function for each seed
     // Make hashx function
 	  if (!hashx_make(ctx->hash_func, seed, 40)) {
 	      return;
 	  }
 
+    // TODO Figure out how many threads and blocks we need based on the batch size
+    // TODO Pass all addresses to the kernel
     // Launch kernel to parallelize hashx operations
     dim3 threadsPerBlock(256); // 256 threads per block
     dim3 blocksPerGrid((65536 + threadsPerBlock.x - 1) / threadsPerBlock.x); // enough blocks to cover 65536 threads
     do_hash_stage0i<<<blocksPerGrid, threadsPerBlock>>>(ctx->hash_func, ctx->heap);
     cudaDeviceSynchronize();
 
+    // TODO Return data and spawn thread to do the remaining stages from rust (cpu)
     // Do the remaining stages
     equix_solution solutions[EQUIX_MAX_SOLS];
     int num_sols = equix_solver_solve(ctx->heap, solutions);
@@ -50,6 +58,8 @@ extern "C" void hash(uint8_t *challenge, uint8_t *nonce, uint8_t *out) {
     }
 }
 
+// TODO Pick correct heap, hash_func, and i 
+// TODO Do many i per thread
 __global__ void do_hash_stage0i(hashx_ctx* hash_func, solver_heap* heap) {
     uint16_t i = blockIdx.x * blockDim.x + threadIdx.x;
     if (i < 65536) {
