94cdfd36719bc2c19377747c88de637c633abe26
Hardhat Chad <hardhatchad@gmail.com>
2024-05-18 18:32:21 +0000

    cuda

--- drillx/cuda/drillx.cu
+++ drillx/cuda/drillx.cu
@@ -8,7 +8,7 @@
 #include "equix/src/solver_heap.h"
 #include "hashx/src/context.h"
 
-const int BATCH_SIZE = 1024;
+const int BATCH_SIZE = 512;
 
 extern "C" void hash(uint8_t *challenge, uint8_t *nonce, uint64_t *out) {
     // Generate a hash function for each (challenge, nonce)
