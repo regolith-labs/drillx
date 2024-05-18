732649cc0f8249f7f90d3962e268e4a661bdfd68
Hardhat Chad <hardhatchad@gmail.com>
2024-05-18 18:27:43 +0000

    cuda

--- drillx/src/cuda.rs
+++ drillx/src/cuda.rs
@@ -33,54 +33,54 @@ mod tests {
                 BATCH_SIZE,
                 timer.elapsed().as_millis()
             );
-
-            // Do memory heavy work on cpu
-            let num_threads = num_cpus::get();
-            let chunk_size = BATCH_SIZE as usize / num_threads;
-            let challenge = Arc::new(challenge);
-            let hashes = Arc::new(hashes);
-            let mut handles = vec![];
-            for t in 0..num_threads {
-                let challenge = challenge.clone();
-                let hashes = hashes.clone();
-                let nonce = u64::from_le_bytes(nonce);
-                let handle = thread::spawn(move || {
-                    let start = t * chunk_size;
-                    let end = if t == num_threads - 1 {
-                        BATCH_SIZE as usize
-                    } else {
-                        start + chunk_size
-                    };
-                    for i in start..end {
-                        let mut digest = [0u8; 16];
-                        let mut sols = [0u8; 4];
-                        let batch_start = hashes.as_ptr().add(i * INDEX_SPACE);
-                        unsafe {
-                            solve_all_stages(
-                                batch_start,
-                                digest.as_mut_ptr(),
-                                sols.as_mut_ptr() as *mut u32,
-                            );
-                        }
-                        if u32::from_le_bytes(sols).gt(&0) {
-                            let solution =
-                                crate::Solution::new(digest, (nonce + i as u64).to_le_bytes());
-                            assert!(solution.is_valid(&challenge));
-                        }
-                    }
-                });
-                handles.push(handle);
-            }
-
-            for handle in handles {
-                handle.join().expect("Failed to join thread");
-            }
-            println!(
-                "Did {} hashes in {} ms",
-                BATCH_SIZE,
-                timer.elapsed().as_millis()
-            );
-            assert!(false);
         }
+
+        // Do memory heavy work on cpu
+        let num_threads = num_cpus::get();
+        let chunk_size = BATCH_SIZE as usize / num_threads;
+        let challenge = Arc::new(challenge);
+        let hashes = Arc::new(hashes);
+        let mut handles = vec![];
+        for t in 0..num_threads {
+            let challenge = challenge.clone();
+            let hashes = hashes.clone();
+            let nonce = u64::from_le_bytes(nonce);
+            let handle = thread::spawn(move || {
+                let start = t * chunk_size;
+                let end = if t == num_threads - 1 {
+                    BATCH_SIZE as usize
+                } else {
+                    start + chunk_size
+                };
+                for i in start..end {
+                    let mut digest = [0u8; 16];
+                    let mut sols = [0u8; 4];
+                    let batch_start = hashes.as_ptr().add(i * INDEX_SPACE);
+                    unsafe {
+                        solve_all_stages(
+                            batch_start,
+                            digest.as_mut_ptr(),
+                            sols.as_mut_ptr() as *mut u32,
+                        );
+                    }
+                    if u32::from_le_bytes(sols).gt(&0) {
+                        let solution =
+                            crate::Solution::new(digest, (nonce + i as u64).to_le_bytes());
+                        assert!(solution.is_valid(&challenge));
+                    }
+                }
+            });
+            handles.push(handle);
+        }
+
+        for handle in handles {
+            handle.join().expect("Failed to join thread");
+        }
+        println!(
+            "Did {} hashes in {} ms",
+            BATCH_SIZE,
+            timer.elapsed().as_millis()
+        );
+        assert!(false);
     }
 }
