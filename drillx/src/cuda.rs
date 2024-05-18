15af6e0595b05522b7a689411d3810391c53dc6b
Hardhat Chad <hardhatchad@gmail.com>
2024-05-18 17:41:33 +0000

    cuda

--- drillx/src/cuda.rs
+++ drillx/src/cuda.rs
@@ -28,10 +28,10 @@ mod tests {
             for i in 0..BATCH_SIZE as usize {
                 let mut digest = [0u8; 16];
                 let batch_start = hashes.as_ptr().add(i * INDEX_SPACE);
+                println!("{} hash: {}", i, batch_start);
                 solve_all_stages(batch_start, digest.as_mut_ptr());
                 let solution = crate::Solution::new(digest, nonce);
-                // assert!(solution.is_valid(&challenge));
-                println!("Digest: {:?}", digest);
+                // println!("Digest: {:?}", digest);
                 println!("{} is valid: {}", i, solution.is_valid(&challenge));
             }
             assert!(false);
